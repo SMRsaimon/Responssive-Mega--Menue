8d254f25061476c4c1896939f1bfbcc0ca7ba2d3
Sultan Mahmud <smrsaimon960@gmail.com>
2021-04-25 10:46:18 +0600

    Mega menue

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+// const numbers = [12, 4, 56, 58, 3, 5, 30, 60, 1, 4]
+
+// for (let i = 0; i < numbers.length; i++) {
+//   for (let j = 0; j < numbers.length; j++) {
+//     if (numbers[j] > numbers[j + 1]) {
+
+//       let teamp = numbers[j]
+//       numbers[j] = numbers[j + 1]
+//       numbers[j + 1] = teamp
+
+//     }
+
+//   }
+
+// }
+// console.log(numbers)
+
+
+class PenDrive {
+  constructor(price, capacity, options) {
+    this.price = price,
+      this.capacity = capacity,
+      this.options = options
+
+
+  }
+}
+const pendrive1 = new PenDrive(136, "56GB", "None")
+
+console.log(pendrive1)
+
+
+ 
